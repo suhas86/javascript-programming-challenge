fdbe3bc89a79a0662ff2684df419b7fadebfe53d
suhas86 <murthy.suhas@gmail.com>
2023-02-14 20:33:36 +0530

    second solution added

--- index.js
+++ index.js
@@ -24,3 +24,41 @@ console.log(output);
 // Write a function twoSum(nums, target) that takes an array of integers nums and an integer target as input and returns the indices of the two numbers that add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // Input: nums = [2, 7, 11, 15], target = 9
 // Output: [0, 1]
+
+/***
+ * Tried Solution
+ */
+
+// function twoSum(nums, target) {
+//   if (!nums.length) return [];
+//   if (nums.length === 1) return nums[0];
+
+//   let result = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     // let count = i;
+//     // while (nums[i]) {}
+//     for (let j = 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return (result = [i, j]);
+//       }
+//     }
+//   }
+//   return result;
+// }
+
+/***
+ * Better Solution
+ *
+ */
+function twoSum(nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+}
+const result = twoSum([2, 7, 11, 15], 9);
+console.log(result);
